510a81fa0b4bd8244f675ecf458fa550c415db2c
Songyan Hou <815759459@qq.com>
2021-03-13 21:57:11 +0100

    add Heston

--- utils.py
+++ utils.py
@@ -16,3 +16,27 @@ def BlackScholes(tau, S, K, sigma, alldata = None):
         return data
     else:
         return price
+    
+    
+def stimulate_Heston(m,Ktrain,N,T,corr,kappa,theta,xi,S0,v0):
+    S = [S0]
+    v = [v0]
+    v_curr = v0; S_curr = S0
+    for i in range(N):
+        # Simulate standard normal random variables
+        Z1_i = np.random.normal(0,1,(Ktrain,m))
+        Z2_i = np.random.normal(0,1,(Ktrain,m))
+        # Generate correlated Brownian motions
+        WS_curr = np.sqrt(T/N)*Z1_i
+        Wv_curr = np.sqrt(T/N)*(corr*Z1_i + np.sqrt(1-corr**2)*Z2_i)
+        # Calculate volatility and stock price
+        # Adjustment: np.abs(v_curr) for np.sqrt() in the last term
+#         v_new = v_curr + kappa*(theta-v_curr)*T/N + xi*np.sqrt(np.abs(v_curr))*Wv_curr
+        v_new = v_curr + kappa*(theta-v_curr)*T/N + xi*np.sqrt(v_curr)*Wv_curr # without abs 
+        S_new = S_curr * np.exp(- v_new**2/2*T/N + v_new*WS_curr)
+        v_curr = v_new; S_curr = S_new
+        # Append the results to the arrays of each day's value
+        v += [v_new]; S += [S_new]
+        price_path = np.swapaxes(np.array(S),0,1)
+        vol_path = np.swapaxes(np.array(v),0,1)
+    return price_path, vol_path
