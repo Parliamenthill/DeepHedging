f9cad61edbf4cd8af3b1edcae6ed2a040ed4b0ab
Songyan Hou <815759459@qq.com>
2021-03-27 17:40:49 +0100

    add

--- utils.py
+++ utils.py
@@ -18,6 +18,10 @@ def BlackScholes(tau, S, K, sigma, option_type):
         price = norm.cdf(d2)
         hedge_strategy = gamma
     return price, hedge_strategy
+
+
+
+
     
 def stimulate_Heston(m,Ktrain,N,T,corr,kappa,theta,xi,S0,v0):
     S = [S0]
@@ -43,13 +47,41 @@ def stimulate_Heston(m,Ktrain,N,T,corr,kappa,theta,xi,S0,v0):
     return price_path, vol_path
 
 
-def stimulate_GBM(m,Ktrain,N,T,sigma,S0):
-    time_grid = np.linspace(0,T,N+1)
-    dt = T/N
-    BM_path_helper = np.cumsum(np.random.normal(size = [Ktrain,N,m], loc=0, scale=np.sqrt(dt)),axis = 1) # generate and sum the increment of BM
+# def stimulate_GBM(m,Ktrain,N,T,sigma,S0):
+#     time_grid = np.linspace(0,T,N+1)
+#     dt = T/N
+#     BM_path_helper = np.cumsum(np.random.normal(size = [Ktrain,N,m], loc=0, scale=np.sqrt(dt)),axis = 1) # generate and sum the increment of BM
+#     BM_path = np.concatenate([np.zeros([Ktrain,1,m]),BM_path_helper],axis = 1) # set initial position of BM be 0 
+#     price_path = S0 * np.exp(sigma * BM_path - 0.5 * sigma **2 * time_grid[None,:,None])  # from BM to geometric BM
+#     return price_path, time_grid
+
+
+def simulate_GBM(m,Ktrain,N,T,sigma,S0, grid_type):
+    if grid_type == 'equi':
+        time_grid = np.linspace(0,T,N+1)
+    elif grid_type == 'exp':
+        time_grid = 1.2**np.arange(0, N+1, 1)
+        time_grid = (time_grid-1)/time_grid
+        time_grid = time_grid/time_grid[-1]*T
+        dt = np.diff(time_grid)
+    elif grid_type == 'equi-exp':
+        N1 = int(N/4)
+        N2 = N - N1
+        T1 = 0.5
+        T2 = 0.5
+        q = 0.97
+        a0 = T2*(q-1)/(q**N2-1)
+        time_grid1 = np.linspace(0,T1,N1+1)
+        time_grid2 = np.cumsum(a0*q**np.arange(0,N2))+T1
+        time_grid = np.concatenate([time_grid1,time_grid2])
+        time_grid
+    dt = np.diff(time_grid)
+    BM_path_helper = np.random.normal(size = (Ktrain,N,m))
+    BM_path_helper = BM_path_helper * np.sqrt(dt)[:,None] # generate and sum the increment of BM
+    BM_path_helper = np.cumsum(BM_path_helper, axis=1) # generate and sum the increment of BM
     BM_path = np.concatenate([np.zeros([Ktrain,1,m]),BM_path_helper],axis = 1) # set initial position of BM be 0 
     price_path = S0 * np.exp(sigma * BM_path - 0.5 * sigma **2 * time_grid[None,:,None])  # from BM to geometric BM
-    return price_path
+    return price_path, time_grid
     
     
 
@@ -80,20 +112,26 @@ def build_network(m, n, d, N):
     return Networks
 
 
-def delta_hedge(price_path,T,K,sigma,option_type):
+def delta_hedge(price_path,payoff, T,K,sigma,option_type,time_grid):
     price = price_path
     batch, N, m = price.shape
     N = N - 1 
-    time_grid = np.linspace(0,T,N+1)
-    
+#     time_grid = np.linspace(0,T,N+1)
     price_difference = price[:,1:,:] - price[:,:-1,:]  
-    hedge = np.zeros_like(price[:,0,:])  
+    
+    hedge_path = np.zeros_like(price)
+    option_path = np.zeros_like(price) 
     premium,_ = BlackScholes(T-time_grid[0], price[:,0,:], K, sigma, option_type)
+    
+    hedge_path[:,0,:] =  premium
+    option_path[:,-1,:] =  payoff
+    
     for j in range(N):
-        _,strategy = BlackScholes(T-time_grid[j],price[:,j,:],K,sigma,option_type)  
-        hedge = hedge + strategy * price_difference[:,j,:]   
-    outputs = premium + hedge 
-    return outputs 
+        option_price, strategy = BlackScholes(T-time_grid[j],price[:,j,:],K,sigma,option_type)  
+        hedge_path[:,j+1] = hedge_path[:,j] + strategy * price_difference[:,j,:]   
+        option_path[:,j,:] =  option_price
+    outputs = hedge_path[:,-1] 
+    return outputs, hedge_path , option_path
     
     
 
